d8bb4cce367dc876e36525050e7b7402ddfa1dba
noganogami <hydetkc7@gmail.com>
2023-02-07 04:59:45 +0900

    change git config

--- make_link.sh
+++ make_link.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-files=(".vimrc" ".tmux.conf" ".gitconfig" ".gitignore_global")
+files=(".vimrc" ".tmux.conf" ".config/git")
 for file in "${files[@]}"
 do
    if [ ! -e ~/$file ];then
