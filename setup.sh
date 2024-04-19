db353fce68c11f202baf8e0d6554bca4aa94be54
noganogami <hydetkc7@gmail.com>
2024-04-20 02:05:42 +0900

    update

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ fi
 files=("vimrc" "tmux.conf" "config/git" "config/nvim" "config/efm-langserver" "vim")
 for file in "${files[@]}"
 do
-   if [ ! -f ~/.$file ];then
+   if [ ! -e ~/.$file ];then
       ln -s ~/dotfiles/$file ~/.$file
    fi
 done
